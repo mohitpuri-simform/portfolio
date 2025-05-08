3a0b9c6609f68dcb954068e46be769b5b8a3d780
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-05-08 13:19:51 +0530

    fix: update the work experience component in main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "./components/Hero Section/HeroSection";
 import Navbar from "./components/Navbar/Navbar";
+import WorkExperience from "./components/Work Experience/WorkExperience";
+import { experiences } from "./constants";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <WorkExperience experiences={experiences} />
     </>
   );
 }
